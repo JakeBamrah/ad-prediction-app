1a9663069199d2e8db01ea852da3fb717fca6981
Jake Bamrah <jakebamrah@gmail.com>
2021-12-27 14:17:33 +0000

    add umap implementation into frontend

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,11 @@
 import React from 'react'
 
 import { AwsClient } from 'aws4fetch'
+import { UMAP } from 'umap-js'
+
+import { EXAMPLE, norm_arr } from './normArr'
 
 
-const EXAMPLE = {
-  "AGE": 84.8186,
-  "CDR": 25,
-  "PTGENDER": 2,
-  "PTEDUCAT": 18,
-  "FATHDEM": 0,
-  "MOTHDEM": 0,
-  "MODHACH_SCORE": 0,
-  "NPITOTAL": 0,
-  "ADAS_TOTAL": 30,
-  "LOG_MEM_IMM_TOTAL": 15,
-  "MMSE": 30,
-  "COMP_MEM_SCORE": 2.033,
-  "COMP_EXEC_FUNC_SCORE": 0.574,
-  "LOG_MEM_DEL_TOTAL": 11,
-  "CBB_SCORE_%": 83.44298245614036,
-}
 async function sign() {
   const aws = new AwsClient({
     service: 'execute-api',
@@ -36,18 +22,28 @@ async function sign() {
     headers: {
       "Content-Type": "application/json",
     },
-    body: JSON.stringify({ patient_to_label: EXAMPLE })
+    body: JSON.stringify({ patient_to_label: EXAMPLE }),
   })
   const data = await response.json()
 
-  console.log(import.meta.env['VITE_AWS_API_URL'])
-  console.log(data.body['predicted_label'])
-  return
+  return data
+}
+
+const buildUMAPEmbeddings = (arr: number[][]) => {
+  const umap = new UMAP({
+    nComponents: 3,
+    nNeighbors: 5,
+    minDist: 0.0,
+  })
+  const embeddings = umap.fit(arr)
+  return embeddings
 }
 
 const App = () => {
-  // this works
-  sign()
+  // sign()
+
+  const n_arr = norm_arr.flat()
+  const embeddings = buildUMAPEmbeddings(n_arr)
 
   return (
     <div>Testing AD prediction api</div>
