a63d1e7d327fa4b8cedb21df352f718049133e1e
Jake Bamrah <jakebamrah@gmail.com>
2022-01-01 12:22:46 +0000

    handle missing request values and update return format

--- engine/src/app.py
+++ engine/src/app.py
@@ -27,25 +27,36 @@ logger.setLevel(logging.INFO)
 
 
 def fill_patient_details(patient_to_predict, df, predict_cols):
-    """Fill in MRI columns using a similar patient for a given patient."""
+    """
+        Fill in MRI columns using a similar patient for a given patient.
+        - Missing values from original request will be replaced by mean.
+        - Remaining MRI values will be filled by most similar patient.
+    """
     cols_to_replace = predict_cols - EXCLUDE_COLS
     reduced = df[cols_to_replace]
 
     # NOTE: make sure columns are in the same order as reduced df
-    patient_to_predict = [patient_to_predict[k] for k in cols_to_replace]
-    patient_to_predict = np.array(patient_to_predict)
-    diff = reduced - patient_to_predict
+    patient_vals = []
+    for col in cols_to_replace:
+        if patient_to_predict[col]:
+            patient_vals.append(patient_to_predict[col])
+        else:
+            patient_vals.append(df[col].mean())
+
+    patient_vals = np.array(patient_vals)
+    diff = reduced - patient_vals
     norm_df = diff.apply(np.linalg.norm, axis=1)
     id = reduced.loc[norm_df.idxmin(skipna=True), :].name
 
     # duplicate similar row and replace with patient to predict values
     # we now have a row that can be used to predict
-    p = dict(zip(cols_to_replace, patient_to_predict))
+    p = dict(zip(cols_to_replace, patient_vals))
     new_row = df.loc[id, :].copy()
     for k,v in p.items():
         new_row[k] = v
 
     return {'new_patient_idx': len(df - 1), 'patient': new_row}
+
 
 def min_max_norm(df, cols_to_exclude=EXCLUDE_COLS):
     """Normalize dataframe for a set of columns."""
@@ -60,10 +71,12 @@ def min_max_norm(df, cols_to_exclude=EXCLUDE_COLS):
     df = (numer / denom)
     return df.fillna(0)
 
+
 def format_df_for_graph(df):
     np_arr = np.expand_dims(df.to_numpy(), axis=1)
     np_arr = np.nan_to_num(np_arr)
     return np_arr
+
 
 def handler(event, context):
     """Lambda handler providing node prediction."""
@@ -91,17 +104,18 @@ def handler(event, context):
     # find similar patient from samples and fill in MRI cols
     df = pd.read_csv('combined.csv')
     filled_p = fill_patient_details(patient_to_label, df, PREDICT_COLS)
+    logger.info(f'Values after fill: {str(filled_p)}')
 
     # patient to predict will always be last row
     df = df.append(filled_p['patient'], ignore_index=True)
 
     norm_df = min_max_norm(df)
-    np_arr = format_df_for_graph(norm_df)
-    unlabelled_node = np_arr[-1]
+    norm_arr = format_df_for_graph(norm_df)
+    unlabelled_node = norm_arr[-1]
 
     # re-organise data for dataloader
     data_dict = defaultdict(list)
-    for i, subj in enumerate(np_arr):
+    for i, subj in enumerate(norm_arr):
         key = int(df['AD_LABEL'][i]) - 1
         data_dict[key].append(subj)
 
@@ -120,17 +134,22 @@ def handler(event, context):
 
     # update unlablled patient with predicted label
     df.loc[len(df) - 1, 'AD_LABEL'] = pred_label
-    df_dict = df[PREDICT_COLS].to_json(orient='records'),
+    df_dict = defaultdict(list)
+    for col in PREDICT_COLS:
+        df_dict[col] = df[col].tolist()
+
+    # reduce array dimension for easier use in frontend
+    norm_arr = np.squeeze(norm_arr, axis=1)
 
     logger.info(f'\
             predicted_label: {pred_label},\
-            df: {df_dict}'
+            data: {df_dict}'
             )
     return {
             'statusCode': 'success',
             'body' : {
                 'predicted_label': int(pred_label),
-                'df': df_dict,
-                'norm_arr': np_arr.tolist()
+                'data': df_dict,
+                'normalized_data': norm_arr.tolist()
                 }
             }
